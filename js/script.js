0460df3751227bc13e3d5414caa562e257a580b0
Valeri Karkashadze <118384756+valerikarakashadze@users.noreply.github.com>
2023-04-28 18:38:09 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
      
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
     menu.onclick = () => {
         navbar.classList.toggle('active');
     }
